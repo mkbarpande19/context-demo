137e598194e202cb32a7d55eeded1b980d6a3300
Mayur Barpande <mayurbarpande9019@gmail.com>
2022-07-20 11:21:42 +0530

    Functional component Ways added

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-export default class ThemeToggle extends Component {
-  static contextType = ThemeContext;
+/* Class Component Pattern */
+// export default class ThemeToggle extends Component {
+//   static contextType = ThemeContext;
 
-  render() {
-    const { toggleTheme } = this.context;
-    return <button onClick={toggleTheme}>Toggle Theme</button>;
-  }
-}
+//   render() {
+//     const { toggleTheme } = this.context;
+//     return <button onClick={toggleTheme}>Toggle Theme</button>;
+//   }
+// }
+
+/* Functional Component Pattern */
+const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  return <button onClick={toggleTheme}>Toggle Theme</button>;
+};
+
+export default ThemeToggle;
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,24 +1,16 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends Component {
-  state = {
-    isAuthenticated: false,
-  };
+const AuthContextProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  toggleAuth = () => {
-    this.setState({ isAuthenticated: !this.state.isAuthenticated });
-  };
-  render() {
-    return (
-      <AuthContext.Provider
-        value={{ ...this.state, toggleAuth: this.toggleAuth }}
-      >
-        {this.props.children}
-      </AuthContext.Provider>
-    );
-  }
-}
+  const toggleAuth = () => setIsAuthenticated(!isAuthenticated);
 
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, toggleAuth: toggleAuth }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContextProvider;
